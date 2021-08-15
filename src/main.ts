e648c8e1e258d4985c9f4d8fde541f10882afb31
Karunpat <jomcootle@gmail.com>
2021-08-15 13:17:15 +0700

    + Add cookieParser and Validate form

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
